a78325d734845cded760986d199e96adfa337466
Marcel Sebald <blazing-star@hotmail.de>
2017-11-18 02:50:11 +0100

    Fix eslint errors

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-import Search from './features/search';
 import 'babel-polyfill';
+import Search from './features/search';
 
 const Telegraf = require('telegraf');
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 Search(bot);
--- initFirebase.js
+++ initFirebase.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase'; // eslint-disable-line
 import config from './apiKey';
 
 const fb = firebase.initializeApp(config);
