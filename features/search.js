2e575760be948a852b60d9e38bd8ea7d7e0500b4
Marcel Sebald <blazing-star@hotmail.de>
2017-11-18 02:45:57 +0100

    fixed some eslint errors

--- features/search.js
+++ features/search.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase'; // eslint-disable-line
 import fb from '../initFirebase';
 
 const database = fb.database();
@@ -6,80 +6,119 @@ const rootRef = fb.database().ref();
 const systemsRef = rootRef.child('games');
 
 // TODO: put in scope, when possible
-let systemKeys = [];
-let systemRefs = [];
-let tmpGames = [];
-let gamesContainer = [];
+const systemKeys = [];
+const systemRefs = [];
+const gamesContainer = [];
 
-let fetchAllGames = systemsRef
-	.once('value', snap => {
-		snap.forEach(child => {
-			let key = child.key;
-			systemKeys.push(key);
-		});
-	})
+const fetchAllGames = systemsRef
+	.once(
+		'value',
+		(snap) => {
+			snap.forEach((child) => {
+				const {
+					key,
+				} = child;
+				systemKeys.push(key);
+			});
+		},
+	)
 	.then(() => {
-		systemKeys.forEach(system => {
-			let systemRef = database.ref(`games/${system}`);
+		systemKeys.forEach((system) => {
+			const systemRef = database.ref(`games/${
+				system
+			}`);
 			systemRefs.push(systemRef);
 		});
 	})
 	.then(() =>
-		Promise.all(gatherGames(systemRefs)).then(res => {
-			return gamesContainer;
-		})
-	);
+		Promise.all(gatherGames(systemRefs)).then(res =>
+			gamesContainer));
 
-let gatherGames = systemRefs => {
-	let promises = systemRefs.map(systemRef => {
-		return systemRef.once('value', snap => {
-			let data = snap.val();
+let gatherGames = () => {
+	const promises = systemRefs.map(systemRef =>
+		systemRef.once(
+			'value',
+			(snap) => {
+				const data = snap.val();
 
-			Object.keys(data).forEach(game => {
-				data[game].key = game;
-				gamesContainer.push(data[game].title);
-			});
-		});
-	});
+				Object.keys(data).forEach((game) => {
+					data[
+						game
+					].key = game;
+					gamesContainer.push(data[
+						game
+					]
+						.title);
+				});
+			},
+		));
 
 	return promises;
 };
 
 function getSearchResults(games, message) {
-	return games.filter(game => game.toLowerCase().includes(message));
+	return games.filter(game =>
+		game
+			.toLowerCase()
+			.includes(message));
 }
 
-const search = bot => {
-	bot.start(ctx => {
-		//console.log('started:', ctx.from.id);
-		return ctx.reply('Welcome!');
-	});
+const search = (bot) => {
+	bot.start(ctx =>
+		// console.log('started:', ctx.from.id);
+		ctx.reply('Welcome!'));
 
-	bot.hears(/.*/gim, ctx => {
-		if ([ctx.message.text.length] < 3) {
-			//console.log([ctx.message.text]);
-			ctx.reply('Please search for at least 3 characters');
-		} else {
-			const message = [ctx.message.text].toString().toLowerCase();
+	bot.hears(
+		/.*/gim,
+		(ctx) => {
+			if (
+				[
+					ctx
+						.message
+						.text
+						.length,
+				] <
+				3
+			) {
+				// console.log([ctx.message.text]);
+				ctx.reply('Please search for at least 3 characters');
+			} else {
+				const message = [
+					ctx
+						.message
+						.text,
+				]
+					.toString()
+					.toLowerCase();
 
-			fetchAllGames.then(allGames => {
-				let searchResults = getSearchResults(allGames, message);
+				fetchAllGames.then((allGames) => {
+					const searchResults = getSearchResults(
+						allGames,
+						message,
+					);
 
-				if (searchResults.length > 0) {
-					ctx.replyWithMarkdown(
-						`FOUND *${searchResults.length}* ${searchResults.length > 1
-							? 'GAMES'
-							: 'GAME'} 💁🏻\n-----\n${searchResults
+					if (
+						searchResults.length >
+							0
+					) {
+						ctx.replyWithMarkdown(`FOUND *${
+							searchResults.length
+						}* ${
+							searchResults.length >
+									1
+								? 'GAMES'
+								: 'GAME'
+						} 💁🏻\n-----\n${searchResults
 							.toString()
 							.split(',')
-							.join('\n')}`
-					);
-				} else {
-					ctx.reply('NO GAMES FOUND! 😱');
-				}
-			});
-		}
-	});
+							.join('\n')}`);
+					} else {
+						ctx.reply('NO GAMES FOUND! 😱');
+					}
+				});
+			}
+		},
+	);
 
 	bot.startPolling();
 };
