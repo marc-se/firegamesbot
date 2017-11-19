e6cb5851345c1bebd2098beb08916f049fc1179d
Marcel Sebald <marc-se@users.noreply.github.com>
2017-11-19 22:07:57 +0100

    Primitive private Bot approach (#2)
    * simple private bot
    
    * Update eslint config

--- features/search.js
+++ features/search.js
@@ -4,6 +4,7 @@ import fb from '../initFirebase';
 const database = fb.database();
 const rootRef = fb.database().ref();
 const systemsRef = rootRef.child('games');
+const OWNER_ID = process.env.OWNER_ID;
 
 // TODO: put in scope, when possible
 const systemKeys = [];
@@ -11,114 +12,74 @@ const systemRefs = [];
 const gamesContainer = [];
 
 const fetchAllGames = systemsRef
-	.once(
-		'value',
-		(snap) => {
-			snap.forEach((child) => {
-				const {
-					key,
-				} = child;
-				systemKeys.push(key);
-			});
-		},
-	)
+	.once('value', snap => {
+		snap.forEach(child => {
+			const { key } = child;
+			systemKeys.push(key);
+		});
+	})
 	.then(() => {
-		systemKeys.forEach((system) => {
-			const systemRef = database.ref(`games/${
-				system
-			}`);
+		systemKeys.forEach(system => {
+			const systemRef = database.ref(`games/${system}`);
 			systemRefs.push(systemRef);
 		});
 	})
 	.then(() =>
-		Promise.all(gatherGames(systemRefs)).then(res =>
-			gamesContainer));
+		Promise.all(gatherGames(systemRefs)).then(res => gamesContainer)
+	);
 
 let gatherGames = () => {
 	const promises = systemRefs.map(systemRef =>
-		systemRef.once(
-			'value',
-			(snap) => {
-				const data = snap.val();
+		systemRef.once('value', snap => {
+			const data = snap.val();
 
-				Object.keys(data).forEach((game) => {
-					data[
-						game
-					].key = game;
-					gamesContainer.push(data[
-						game
-					]
-						.title);
-				});
-			},
-		));
+			Object.keys(data).forEach(game => {
+				data[game].key = game;
+				gamesContainer.push(data[game].title);
+			});
+		})
+	);
 
 	return promises;
 };
 
 function getSearchResults(games, message) {
-	return games.filter(game =>
-		game
-			.toLowerCase()
-			.includes(message));
+	return games.filter(game => game.toLowerCase().includes(message));
 }
 
-const search = (bot) => {
+const search = bot => {
 	bot.start(ctx =>
 		// console.log('started:', ctx.from.id);
-		ctx.reply('Welcome!'));
+		ctx.reply('Welcome!')
+	);
 
-	bot.hears(
-		/.*/gim,
-		(ctx) => {
-			if (
-				[
-					ctx
-						.message
-						.text
-						.length,
-				] <
-				3
-			) {
+	bot.hears(/.*/gim, ctx => {
+		if (ctx.message.from.id.toString() === OWNER_ID) {
+			if ([ctx.message.text.length] < 3) {
 				// console.log([ctx.message.text]);
 				ctx.reply('Please search for at least 3 characters');
 			} else {
-				const message = [
-					ctx
-						.message
-						.text,
-				]
-					.toString()
-					.toLowerCase();
+				const message = [ctx.message.text].toString().toLowerCase();
 
-				fetchAllGames.then((allGames) => {
-					const searchResults = getSearchResults(
-						allGames,
-						message,
-					);
+				fetchAllGames.then(allGames => {
+					const searchResults = getSearchResults(allGames, message);
 
-					if (
-						searchResults.length >
-							0
-					) {
-						ctx.replyWithMarkdown(`FOUND *${
-							searchResults.length
-						}* ${
-							searchResults.length >
-									1
-								? 'GAMES'
-								: 'GAME'
-						} 💁🏻\n-----\n${searchResults
-							.toString()
-							.split(',')
-							.join('\n')}`);
+					if (searchResults.length > 0) {
+						ctx.replyWithMarkdown(
+							`FOUND *${searchResults.length}* ${
+								searchResults.length > 1 ? 'GAMES' : 'GAME'
+							} 💁🏻\n-----\n${searchResults
+								.toString()
+								.split(',')
+								.join('\n')}`
+						);
 					} else {
 						ctx.reply('NO GAMES FOUND! 😱');
 					}
 				});
 			}
-		},
-	);
+		}
+	});
 
 	bot.startPolling();
 };
