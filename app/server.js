e5c3568e902c46285c2cac547798bd64a7bd073e
Marcel Sebald <blazing-star@hotmail.de>
2018-01-19 16:41:08 +0100

    Dyno wakup call

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
-var express = require('express');
+const express = require('express');
+const http = require('http');
 
 const app = express();
 const port = process.env.PORT || 3030;
@@ -6,6 +7,13 @@ const port = process.env.PORT || 3030;
 app.get('/', function(req, res) {
 	res.send('Server running ✅');
 });
+
+setInterval(
+	function() {
+		http.get('http://<your app name>.herokuapp.com');
+	},
+	900000
+); // wake up call every 15 minutes
 
 const server = app.listen(port, '0.0.0.0', function() {
 	console.info('Web server running at http://0.0.0.0:%s/ ✅', port);
