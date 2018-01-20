e235f3be0f3cfc7e5929eb23413a01cb52e6afbe
Marcel Sebald <blazing-star@hotmail.de>
2018-01-20 16:53:04 +0100

    Update awake function

--- app/server.js
+++ app/server.js
@@ -8,12 +8,33 @@ app.get('/', function(req, res) {
 	res.send('Server running ✅');
 });
 
-setInterval(
-	function() {
-		http.get(`http://${process.env.HEROKU_APP_NAME}.herokuapp.com`);
-	},
-	900000
-); // wake up call every 15 minutes
+function startKeepAlive() {
+	setInterval(
+		function() {
+			let options = {
+				host: `http://${process.env.HEROKU_APP_NAME}.herokuapp.com`,
+				port: 80,
+				path: '/',
+			};
+			http
+				.get(options, function(res) {
+					res.on('data', function(chunk) {
+						try {
+							console.log('HEROKU RESPONSE: ' + chunk);
+						} catch (err) {
+							console.log(err.message);
+						}
+					});
+				})
+				.on('error', function(err) {
+					console.log('Error: ' + err.message);
+				});
+		},
+		20 * 60 * 1000
+	); // ping every 20 minutes
+}
+
+startKeepAlive();
 
 const server = app.listen(port, '0.0.0.0', function() {
 	console.info('Web server running at http://0.0.0.0:%s/ ✅', port);
