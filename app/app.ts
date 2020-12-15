a2d2078f435eb1a490a85d722e508e070814a753
Marcel Sebald <blazing-star@hotmail.de>
2020-12-15 22:59:41 +0100

    feat: add wishlist command

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,8 @@
 import Search from "./features/search";
 import Statistics from "./features/statistics";
 import Systems from "./features/systems";
+import Wishlist from "./features/wishlist";
+
 const Telegraf = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -8,6 +10,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 // commands
 Statistics(bot);
 Systems(bot);
+Wishlist(bot);
 
 // hears
 Search(bot);
--- app/features/wishlist.ts
+++ app/features/wishlist.ts
@@ -1,26 +1,51 @@
 import { isAuthorizedUser } from "../utils/isAuthorizedUser";
 import { fetchWishlist } from "../net/fetchWishlist";
+import type { WishlistItem } from "../types";
 
-const getWishlsit = (ctx: any) => {
+const sendReplyMessage = (
+  ctx: any,
+  gameArray: WishlistItem[],
+  withTitle: boolean = true
+) => {
+  let replyMessage = "";
+  gameArray.map((game) => {
+    if (!game.purchased) {
+      replyMessage = `${replyMessage}\n- ${game.title} (${game.region})`;
+    }
+  });
+  if (withTitle) {
+    ctx.replyWithMarkdown(`*Wishlist 🎁:*${replyMessage}`);
+    return;
+  }
+  ctx.replyWithMarkdown(`${replyMessage}`);
+};
+
+const getWishlist = (ctx: any) => {
   const userId = ctx.message.from.id.toString();
 
-  fetchWishlist()
-    .then((wishlist) => {
-      if (wishlist) {
-        const replyMessage = wishlist.map(
-          (game) => `- ${game.title} (${game.system} | ${game.region})\n`
-        );
-        if (isAuthorizedUser(userId)) {
-          ctx.replyWithMarkdown(`*Wishlist 🎁:*${replyMessage}`);
+  if (isAuthorizedUser(userId)) {
+    fetchWishlist()
+      .then((wishlist) => {
+        if (wishlist) {
+          if (wishlist.length > 60) {
+            const half = Math.ceil(wishlist.length / 2);
+            const firstHalf = wishlist.splice(0, half);
+            sendReplyMessage(ctx, firstHalf);
+            const secondHalf = wishlist.splice(-half);
+            sendReplyMessage(ctx, secondHalf, false);
+            return;
+          }
+
+          sendReplyMessage(ctx, wishlist);
         }
-      }
-    })
-    .catch((error) => console.error("Wishlist Error", error));
+      })
+      .catch((error) => console.error("Wishlist Error", error));
+  }
 };
 
 const wishlist = (bot: any) => {
-  bot.command("/wishlist", (ctx: any) => getWishlsit(ctx));
-  bot.hears(/wishlist/gim, (ctx: any) => getWishlsit(ctx));
+  bot.command("/wishlist", (ctx: any) => getWishlist(ctx));
+  bot.hears(/wishlist/gim, (ctx: any) => getWishlist(ctx));
 };
 
 export default wishlist;
