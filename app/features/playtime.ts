baf0f8fd89f718307cbdbfc003fb7df0f5966f5f
Marcel Sebald <blazing-star@hotmail.de>
2021-06-11 23:04:48 +0200

    wip: playtime feature

--- app/features/playtime.ts
+++ app/features/playtime.ts
@@ -3,14 +3,20 @@ const { Markup } = require("telegraf");
 import { isAuthorizedUser } from "../utils/isAuthorizedUser";
 
 import { fetchAllGames } from "../net/fetchAllGames";
-import type { GameReference } from "../types";
+import { fetchSystemShortNames } from "../net/fetchSystemShortNames";
+import type { GameReference, System } from "../types";
+
+const NodeCache = require("node-cache");
+const botCache = new NodeCache({ stdTTL: 300, checkperiod: 320 });
+
+// TODO: cache all fetched games (unfiltered)
 
 const getReplyMessage = (
   games: GameReference[],
+  systems: System[] = [],
   lowerInterval: number,
   upperInterval: number
-) => {
-  // TODO
+): string[] => {
   let introMessage = "";
   if (lowerInterval && upperInterval) {
     introMessage = `Games between ${lowerInterval} and ${upperInterval} hours playtime:\n`;
@@ -20,7 +26,47 @@ const getReplyMessage = (
     introMessage = `Games with more than ${lowerInterval} hours playtime:\n`;
   }
 
-  return `${introMessage}`;
+  if (games.length > 100) {
+    const half = Math.round(games.length / 2);
+    const firstChunk = games.slice(0, half);
+    const secondChunk = games.slice(half);
+
+    let firstChunkReply = "";
+    let secondChunkReply = "";
+
+    firstChunk.forEach((game) => {
+      const systemNode = systems.find((system) => system.url === game.parent);
+      if (systemNode && systemNode.alias) {
+        firstChunkReply = `${firstChunkReply}\n- ${game.title} (${systemNode.alias}, ${game.playtime}h)`;
+        return;
+      }
+      firstChunkReply = `${firstChunkReply}\n- ${game.title} (${game.playtime}h)`;
+    });
+
+    secondChunk.forEach((game) => {
+      const systemNode = systems.find((system) => system.url === game.parent);
+      if (systemNode && systemNode.alias) {
+        secondChunkReply = `${secondChunkReply}\n- ${game.title} (${systemNode.alias}, ${game.playtime}h)`;
+        return;
+      }
+      secondChunkReply = `${secondChunkReply}\n- ${game.title} (${game.playtime}h)`;
+    });
+
+    return [`${introMessage}${firstChunkReply}`, secondChunkReply];
+  }
+
+  let replyMessage = "";
+
+  games.forEach((game) => {
+    const systemNode = systems.find((system) => system.url === game.parent);
+    if (systemNode && systemNode.alias) {
+      replyMessage = `${replyMessage}\n- ${game.title} (${systemNode.alias}, ${game.playtime}h)`;
+      return;
+    }
+    replyMessage = `${replyMessage}\n- ${game.title} (${game.playtime}h)`;
+  });
+
+  return [`${introMessage}${replyMessage}`];
 };
 
 const handlePlaytimeOption = (
@@ -28,8 +74,6 @@ const handlePlaytimeOption = (
   lowerInterval: number = 0,
   upperInterval: number = 0
 ) => {
-  console.log("handlePlaytimeOption", lowerInterval, upperInterval);
-
   fetchAllGames()
     .then((res) => {
       if (res) {
@@ -69,14 +113,32 @@ const handlePlaytimeOption = (
           );
         }
 
+        const cachedSystems = botCache.get("allSystems");
+
+        if (cachedSystems == undefined) {
+          fetchSystemShortNames().then((systems) => {
+            botCache.set("allSystems", systems);
+            const replyMessage = getReplyMessage(
+              filteredGames,
+              systems,
+              lowerInterval,
+              upperInterval
+            );
+
+            replyMessage.forEach((reply) => ctx.replyWithMarkdown(reply));
+          });
+          return;
+        }
+
         const replyMessage = getReplyMessage(
           filteredGames,
+          cachedSystems,
           lowerInterval,
           upperInterval
         );
 
-        // TODO: find suitable message
-        ctx.replyWithMarkdown(replyMessage);
+        replyMessage.forEach((reply) => ctx.replyWithMarkdown(reply));
+
         return;
       }
     })
@@ -87,11 +149,16 @@ const getGamesByPlaytime = (ctx: any, bot: any) => {
   const userId = ctx.message.from.id.toString();
   if (isAuthorizedUser(userId)) {
     ctx.replyWithMarkdown(
-      `*Choose an option*`,
+      `*Choose your prefered playtime*`,
       Markup.inlineKeyboard([
-        [Markup.button.callback("playtime < 8h", "playtime_lt_8")],
-        [Markup.button.callback("playtime > 8h < 20h", "playtime_gt_8_lt_20")],
-        [Markup.button.callback("playtime > 20h", "playtime_gt_20h")],
+        [Markup.button.callback("playtime less than 8h", "playtime_lt_8")],
+        [
+          Markup.button.callback(
+            "playtime between 8h and 20h",
+            "playtime_gt_8_lt_20"
+          ),
+        ],
+        [Markup.button.callback("playtime more than 20h", "playtime_gt_20h")],
       ])
     );
   }
